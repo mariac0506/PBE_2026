7b44366816d35e93f10e8d9a41b36da1333f88df
MARIA CLARA BENTO DOS SANTOS <maria.santos361@portalsesisp.org.br>
2026-08-06 15:12:00 -0300

    15hr 06/08

---
+++ 01Aula/01_revisao/atividade_1.php
@@ -0,0 +1,10 @@
+<?php 
+    $nome = "maria clara";
+    $escola = "SESI";
+
+    echo "meu nome é $nome!";
+    echo"estudo na escola $escola .";
+
+?>
+
+
---
+++ 01Aula/01_revisao/atividade_5.php
@@ -0,0 +1,15 @@
+<?php
+
+$numero = [2, 4, 6, 8, 10];
+$soma = 0;
+
+foreach($numero as $numero){
+    $soma += $numero;
+}
+
+echo "soma = ". $soma;
+
+?>
+
+
+
