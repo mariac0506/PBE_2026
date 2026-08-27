e9a37b5dc26b33b9894bd65aa8f6e8a866b518ed
MARIA CLARA BENTO DOS SANTOS <maria.santos361@portalsesisp.org.br>
2026-08-27 13:47:35 -0300

    27/08/2026 13hr50

---
+++ desafio/atividade.php/atividade_5.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once "funcao.php";
+
+$resultadocalcularPedido("teclado",100,10,5,7);
+echo "nome: ".$resultado['nomeProduto']."<br>";
+echo "sunTotal: ".$resultado['subTotal']."<br>";
+echo "desconto: ".$resultado['valorDesconto']."<br>";
+echo "total: ".$resultado['totalFinal']."<br>";
+
+?>
---
+++ desafio/funcao.php/atividade_5.php
@@ -0,0 +1,29 @@
+<?php
+
+funcation calcularPedido($nome , $preco, $quantidade, $desconto = 0, $imposto = 0 ){
+    $subTotal = $preco * $quantidade;
+    $valorDesconto = $subTotal * ($desconto/100);
+    $valorTotalComDesconto = $subtotal - $valorDesconto;
+    $valorImposto = $valorTotalComDesconto * ($imposto/100);
+    $totalFinal = $valorTotalComDesconto + $valorImposto;
+
+
+
+    return [
+        "nomeProduto" => $nome,
+        "subTotal" => $subTotal,
+        "valorDesconto" => $valorDesconto,
+        "valorImposto" => $valorImposto,
+        "totalFinal" => $totalFinal
+        
+    ];
+
+
+}
+
+
+
+
+
+
+?>
